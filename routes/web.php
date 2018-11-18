a61628c3cb7ae90dc8b082f18574f6702f3db3a6
Pirakavi <sspirakavi@gmail.com>
2018-11-18 20:48:31 +0530

    local
    local change

---
+++ routes/web.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/index', function () {
+    return view('index');
+});
+
+Route::get('/dashboard', function () {
+    return view('Admin/dashboard');
+});
+
+Route::get('/login', function () {
+    return view('Admin/login');
+});
+
+Route::get('/adminRegister', function () {
+    return view('Admin/adminRegister');
+});
+
+Route::get('/adminOrders', function () {
+    return view('Admin/adminOrders');
+});
+
+Route::get('/adminPatterns', function () {
+    return view('Admin/adminPatterns');
+});
+
+Route::get('/adminMeasurements', function () {
+    return view('Admin/adminMeasurements');
+});
+
+Route::get('/adminStates', function () {
+    return view('Admin/adminStates');
+});
